e426dfcf601ca7da3a81069db97a7ad9b60e93ce
delfinblaksleymujica <delf.blaksley@hotmail.com>
2022-08-22 10:49:08 -0300

    Agregado de precio en el aviso de cierre de compra

--- js/compraFinalizada.js
+++ js/compraFinalizada.js
@@ -6,7 +6,9 @@ const informacionCompra = document.getElementById("compraFinalizada__informacion
 
 /*Traer valor de compra para mostrar*/
 
-let valorDeCompra = localStorage.getItem("ValorDeComra");
+let valorDeCompra = localStorage.getItem("ValorDeCompra");
+
+console.log(valorDeCompra);
 
 /*Card dinamica con informacion de compra*/
 
@@ -14,7 +16,7 @@ const { nombre, email, domicilio , numeroDeTarjeta } = informacionDeCompra
 
 html = `<div class="animate__animated animate__jackInTheBox ticket__container">
             <h2>¡Gracias <b>${nombre}</b> por tu compra!</h2>
-            <h3>¡El pago de <b>$FALTA EL VALOR ACA</b> ya fue aprobado con la tarjeta <b>${numeroDeTarjeta} </b>¡Que los disfrutes!</h3>
+            <h3>¡El pago de <b>$${valorDeCompra} </b> ya fue aprobado con la tarjeta <b>${numeroDeTarjeta} </b>¡Que los disfrutes!</h3>
             <p>En las próximas 24hs vas a estar recibiendo un mail a <b>${email}</b> con toda la información restante de la compra y la información que necestias tener para la recepción de tu pedido. Además en el rango de 7 días van a llegar tus productos a <b>${domicilio}</b>.</p>
             <p>¡Que tengas buen día!</p>`
 
